9d8f543208c2e86610cdee4e62e05864377d0d6f
Rodrigo Guerrero <roguerrero.go@gmail.com>
2022-06-06 14:39:45 -0400

    first commit

---
+++ src/test/java/cl/jagerbomb/unittest/UnitTestApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.jagerbomb.unittest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
